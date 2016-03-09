21cead29fa56c8b9a3ef7d5e66e4330ecff1537c
Josh Morrow <josh@jcmorrow.com>
2016-03-09 10:46:31 -0800

    Making forms look better and still be accessible

---
+++ app/assets/javascripts/answer.js
@@ -0,0 +1,20 @@
+var answer_ready = function(event) {
+  labels = $('.answer_choice .radio label');
+
+  labels.children('input[checked]').closest('label').addClass('selected');
+
+  labels.click(function(event) {
+    labels.removeClass('selected');
+    target = $(event.target);
+    target.closest('label').addClass('selected');
+  })
+
+  labels.children('input[type="radio"]').focus(function(event) {
+    $(event.target).closest('label').addClass('label-focus');
+  }).blur(function(event) {
+    $(event.target).closest('label').removeClass('label-focus');
+  });
+}
+
+$(document).ready(answer_ready);
+$(document).on('page:load', answer_ready);
