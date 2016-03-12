a38c14ea650aab2dba2cca3ea6d0803de5b32bb3
Josh Morrow <josh@jcmorrow.com>
2016-03-12 09:34:44 -0800

    Mostly small style changes and displaying 'guest' for guest users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,11 @@ module ApplicationHelper
     elsif current_user.nil?
       link_to "Log In", new_user_session_path
     else
-      current_user.email
+      if(current_user.guest?)
+        "Guest"
+      else
+        current_user.email
+      end
     end
   end
 end
