382f610d0c71d67989dc5f993980a40baf920e86
Josh Morrow <josh@jcmorrow.com>
2016-03-14 11:42:54 -0700

    Fixing security vulnerability with logging in as admin

--- app/controllers/simple_registrations_controller.rb
+++ app/controllers/simple_registrations_controller.rb
@@ -2,7 +2,7 @@ class SimpleRegistrationsController < ApplicationController
   def create
     @user = User.from_email(user_params[:email])
     @quiz = Quiz.friendly.find(params[:quiz_id])
-    if(@user.valid?)
+    if(@user.valid? && !@user.admin?)
       sign_in(:user, @user)
       redirect_to new_question_answer_path(@quiz.first_question)
     else
